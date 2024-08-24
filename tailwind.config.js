b477cbbd0c0a6d713ad5fe8d85638943f6b06ad5
Md. Abu Sakib Chowdhury <sakib21eee@gmail.com>
2024-08-24 11:51:50 +0600

    content updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,7 @@ module.exports = {
     content: ["./src/**/*.{html,js}"],
     theme: {
       extend: {
-        backgroundImage: {
-          'texture': "url(./assets/bg.jpg)",
-        }
+        
       },
     },
     plugins: [],
